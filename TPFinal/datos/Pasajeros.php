dcacd3b6cc10c3322b53be3a3c747e198aa51b64
DarioFuentealba <dario.fuentealba@est.fi.uncoma.edu.ar>
2024-06-08 18:09:06 -0300

    Modificaciones varias en Pasajero y Persona

--- TPFinal/datos/Pasajeros.php
+++ TPFinal/datos/Pasajeros.php
@@ -1,62 +1,87 @@
 <?php
+/*
+TODO: N° de linea aproximado --> Falla que veo --> Linea original para saber cual es si es que no esta en la misma linea
 
+? Lineas 45 y 64 --> Error con getter y setter de mensajeOperacion
+
+? Linea 107 --> No se bien si eso está bien --> @param STRING $condicion
+
+? Linea 115 --> Esta duda la teniamos que consultar --> $consulta = "select * from pasajero "; //Pusimos select * from pasajero pero no sabemos si es SELECT * FROM pasajero 
+
+? Linea 138 --> Está para probar supongo, cuando no se necesite mas hay que sacarlo --> //echo $consultaPersonas;
+
+? Lineas 91, 136, 174, 200  --> Comentarios que no estoy 100% seguro de que sea eso lo que hace --> //Si se conecta a la base de datos
+
+? Lineas 94, 139, 177, 203 --> Comentarios que no estoy 100% seguro de que sea eso lo que hace --> //Si se ejecuta la consulta
+
+
+? Linea 215 --> Falta el metodo modificar
+*/
+
+//! ******** CREO LA CLASE ******** 
 class Pasajero extends Persona
 {
-//LALALALALALALALALLALALALA
-    //!ATRIBUTOS
+
+	//! ******** ATRIBUTOS ******** 
     private $idViaje;
     private $telefono;
 
-    //!CONSTRUCTOR
+    //! ******** CONSTRUCTOR ******** 
     public function __construct()
     {
         parent::__construct();
-        $this->idViaje =    0;
+        $this->idViaje = 0;
         $this->telefono = 0;
     }
 
+	//! ******** Creo la funcion cargar ******** 
     public function cargar($NroD, $Nom, $Ape, $idViaje = null, $telefono = null){	
-	    parent::cargar($NroD, $Nom, $Ape);
-	    $this->setIdViaje($idViaje);
+		parent::cargar($NroD, $Nom, $Ape);
+		$this->setIdViaje($idViaje);
         $this->setTelefono($telefono);
     }
 
-    //! **********GETTERS*************
 
-
-    public function getTelefono()
-    {
-        return $this->telefono;
-    }
-    public function getIdViaje()
-    {
-        return $this->idViaje;
-    }
-
-    public function getMensajeoperacion()
-    {
-        return $this->mensajeoperacion;
-    }
-
-    //! **********SETTERS**********
-
-
+    //! ********** SETTERS **********
+    //Setea el valor de idViaje
     public function setIdViaje($id)
     {
-        $this->idViaje=$id;
+        $this->idViaje = $id;
     }
 
+    //Setea el valor de telefono
     public function setTelefono($telefono)
     {
         $this->telefono = $telefono;
     }
 
-    public function setMensajeoperacion($mensajeoperacion){
-		$this->mensajeoperacion=$mensajeoperacion;
+    //Setea el valor de mensajeOperacion
+    public function setMensajeOperacion($mensajeOperacion){
+		$this->mensajeOperacion = $mensajeOperacion;
 	}
 
-    //! ******** __toString *********
 
+    //! ********** GETTERS *************
+	//Obtiene el valor de telefono
+    public function getTelefono()
+    {
+        return $this->telefono;
+    }
+
+	//Obtiene el valor de idViaje
+    public function getIdViaje()
+    {
+        return $this->idViaje;
+    }
+
+    //Obtiene el valor de mensajeOperacion
+    public function getMensajeOperacion()
+    {
+        return $this->mensajeOperacion;
+    }
+
+
+    //! ******** __toString *********
     public function __toString()
     {
         $cadena= parent:: __toString() . "\n";
@@ -65,96 +90,170 @@ class Pasajero extends Persona
         return $cadena;
     }
 
+
+	//! ********* BUSCAR *********
     /**
-	 * Recupera los datos de una persona por dni
-	 * @param int $dni
-	 * @return true en caso de encontrar los datos, false en caso contrario 
+	 * Recupera los datos de un Pasajero por medio de su numero de dni que se ingresa como parametro.
+	 * Retorna true en caso de encontrar los datos, false en caso contrario.
+	 * @param INT $dni
+	 * @return BOOLEAN $resp 
 	 */		
     public function Buscar($dni){
-		$base=new BaseDatos();
-		$consulta="Select * from estudiante where nrodoc=".$dni;
-		$resp= false;
-		if($base->Iniciar()){
-		    if($base->Ejecutar($consulta)){
-				if($row2=$base->Registro()){	
-				    parent::Buscar($dni);
-				    $this->setIdViaje($row2['idviaje']);
-                    $this->setTelefono($row2['ptelefono']);
-					$resp= true;
-				}				
-		 	}else{
-		 		$this->setMensajeoperacion($base->getError());
-			}
-		 }else{
-		 	$this->setMensajeoperacion($base->getError());
-		 }		
-		 return $resp;
-	}
+		$base = new BaseDatos();
+		$consulta = "Select * from estudiante where nrodoc = ".$dni;
+		$resp = false;
 
-    public static function listar($condicion=""){
-	    $arreglo = null;
-		$base=new BaseDatos();
-		$consulta="select * from pasajero ";  //Pusimos select * from pasajero pero no sabemos si es SELECT * FROM pasajero
-		if ($condicion!=""){
-		    $consulta=$consulta.' where '.$condicion;
-		}
-		$consulta.=" order by apellido ";
-		//echo $consultaPersonas;
+		//Si se conecta a la base de datos
 		if($base->Iniciar()){
-		    if($base->Ejecutar($consulta)){				
-			    $arreglo= array();
-				while($row2=$base->Registro()){
-					$obj=new Pasajero();
-					$obj->Buscar($row2['nrodoc']);
-					array_push($arreglo,$obj);
-				}
-		 	}else{
-		 		$this->setMensajeoperacion($base->getError());
+
+			//Si se ejecuta la consulta
+			if($base->Ejecutar($consulta)){
+
+				//
+				if($row2 = $base->Registro()){	
+					parent::Buscar($dni);
+					$this->setIdViaje($row2['idviaje']);
+                    $this->setTelefono($row2['ptelefono']);
+					$resp = true;
+				}	
+
+			}else{ //Si no se ejecuta la consulta
+				$this->setMensajeOperacion($base->getError());
 			}
-		 }else{
-		 	$this->setMensajeoperacion($base->getError());
-		 }	
-		 return $arreglo;
-	}
-	
-	public function insertar(){
-		$base=new BaseDatos();
-		$resp= false;
-		
-		if(parent::insertar()){
-		    $consultaInsertar="INSERT INTO pasajero(nroDoc, idviaje, telefono)
-				VALUES (".parent::getNrodoc().",".$this->getIdViaje().",'".$this->getTelefono()."')";
-		    if($base->Iniciar()){
-		        if($base->Ejecutar($consultaInsertar)){
-		            $resp=  true;
-		        }else{
-		            $this->setMensajeoperacion($base->getError());
-		        }
-		    }else{
-		        $this->setMensajeoperacion($base->getError());
-		    }
-		 }
+
+		}else{ //Si no se conecta a la base de datos
+			$this->setMensajeOperacion($base->getError());
+		}		
 		return $resp;
 	}
 
-    /**
-     * Metodo para eliminar 
-     */
 
-    public function eliminar(){
-		$base=new BaseDatos();
-		$resp=false;
+    //! ******** LISTAR ******** 
+    /**
+	 * Lista los datos de todos los pasajeros por medio de una consulta ingresada como parametro.
+	 * Retorna un array con los datos obtenidos.
+	 * @param STRING $condicion
+	 * @return ARRAY $arreglo 
+	 */		
+	public static function listar($condicion = ""){
+		//Inicializo variables
+		$arreglo = null;
+		$base = new BaseDatos();
+
+		//Asigno valor a la consulta y la trabajo con la condicional if
+		$consulta = "select * from pasajero ";
+		/*
+		TODO: Pusimos select * from pasajero pero no sabemos si es SELECT * FROM pasajero 
+		*/
+
+		if ($condicion != ""){
+			$consulta = $consulta.' where '.$condicion;
+		}
+
+		//
+		$consulta .= " order by apellido ";
+
+		//echo $consultaPersonas;
+		//Si se conecta a la base de datos
 		if($base->Iniciar()){
-			$consultaBorra="DELETE FROM pasajero WHERE nrodoc=".parent::getNrodoc();
+
+			//Si se ejecuta la consulta
+			if($base->Ejecutar($consulta)){				
+				$arreglo = array();
+
+				//
+				while($row2 = $base->Registro()){
+					$obj = new Pasajero();
+					$obj->Buscar($row2['nrodoc']);
+					//Agrego el pasajero al array
+					array_push($arreglo,$obj);
+				}
+
+			}else{ //Si no se ejecuta la consulta
+				$this->setMensajeOperacion($base->getError());
+			}
+
+		}else{ //Si no se conecta a la base de datos
+			$this->setMensajeOperacion($base->getError());
+		}
+		return $arreglo;
+	}
+
+
+    //! ******** INSERTAR ******** 
+    /**
+	 * Inserta un nuevo pasajero a la coleccion de pasajeros.
+	 * Retorna true si pudo insertarlo y false en caso contrario.
+	 * @return BOOLEAN $resp 
+	 */		
+	public function insertar(){
+		//Inicializo variables
+		$base = new BaseDatos();
+		$resp = false;
+		
+		//
+		if(parent::insertar()){
+			$consultaInsertar="INSERT INTO pasajero(nroDoc, idviaje, telefono)
+				VALUES (".parent::getNrodoc().", ".$this->getIdViaje().", '".$this->getTelefono()."')";
+
+			//Si se conecta a la base de datos
+			if($base->Iniciar()){
+
+				//Si se ejecuta la consulta
+				if($base->Ejecutar($consultaInsertar)){
+					$resp=  true;
+
+				}else{ //Si no puede ejecutar la consulta
+					$this->setMensajeOperacion($base->getError());
+				}
+
+			}else{ //Si no puede conectarse a la base de datos
+				$this->setMensajeOperacion($base->getError());
+			}
+		}
+		return $resp;
+	}
+
+
+    //! ******** MODIFICAR ******** 
+	/**
+	 * Modifica un pasajero de la coleccion de pasajeros.
+	 * Retorna true si pudo modificarla y false en caso contrario.
+	 * @return BOOLEAN $resp 
+	 */		
+    public function modificar()
+    {
+
+	}
+
+
+	//! ******** ELIMINAR ******** 
+    /**
+	 * Elimina un pasajero a la coleccion de pasajeros.
+	 * Retorna true si pudo eliminarlo y false en caso contrario.
+	 * @return BOOLEAN $resp 
+	 */		
+    public function eliminar(){
+		//Inicializo variables
+		$base = new BaseDatos();
+		$resp = false;
+
+		//Si se conecta a la base de datos
+		if($base->Iniciar()){
+			$consultaBorra = "DELETE FROM pasajero WHERE nrodoc = ".parent::getNrodoc();
+
+			//Si se ejecuta la consulta
 			if($base->Ejecutar($consultaBorra)){
 				if(parent::eliminar()){
-				    $resp=  true;
+					$resp = true;
 				}
-			}else{
-				$this->setMensajeoperacion($base->getError());
+
+			}else{ //Si no puede ejecutar la consulta
+				$this->setMensajeOperacion($base->getError());
 			}
-		}else{
-			$this->setMensajeoperacion($base->getError());
+
+		}else{ //Si no puede conectarse a la base de datos
+			$this->setMensajeOperacion($base->getError());
 		}
 		return $resp; 
 	}
--- TPFinal/datos/Persona.php
+++ TPFinal/datos/Persona.php
@@ -1,22 +1,28 @@
 <?php
+/*
+? Mismos cambios que en pasajeros
 
+? funcion cargar --> me molestan las variables que comienzan con mayusculas, hay que consultar, porque estaban así en lo que mandaron de ejemplo. Ademas, se usan con mayusculas en los setter pero no en el __construct ni en los getters
+*/
+//! ******** CREO LA CLASE ******** 
 class Persona
 {
-
+	//! ******** ATRIBUTOS ******** 
     private $nrodoc;
     private $nombre;
     private $apellido;
-    private $mensajeoperacion;
+    private $mensajeOperacion;
 
+    //! ******** CONSTRUCTOR ******** 
     public function __construct()
     {
         $this->nrodoc = "";
         $this->nombre = "";
         $this->apellido = "";
-        $this->mensajeoperacion = "";
-      
+        $this->mensajeOperacion = "";
     }
 
+	//! ******** Creo la funcion cargar ******** 
     public function cargar($NroD, $Nom, $Ape)
     {
         $this->setNrodoc($NroD);
@@ -24,84 +30,134 @@ class Persona
         $this->setApellido($Ape);
     }
 
+
+    //! ********** SETTERS *************
+	//Setea el valor de NroDNI
     public function setNrodoc($NroDNI)
     {
         $this->nrodoc = $NroDNI;
     }
+
+	//Setea el valor de Nom
     public function setNombre($Nom)
     {
         $this->nombre = $Nom;
     }
+
+	//Setea el valor de Ape
     public function setApellido($Ape)
     {
         $this->apellido = $Ape;
     }
 
-
-    public function setmensajeoperacion($mensajeoperacion)
+	//Setea el valor de mensajeOperacion
+    public function setMensajeOperacion($mensajeOperacion)
     {
-        $this->mensajeoperacion = $mensajeoperacion;
+        $this->mensajeOperacion = $mensajeOperacion;
     }
 
+
+    //! ********** GETTERS *************
+	//Obtiene el valor de nrodoc
     public function getNrodoc()
     {
         return $this->nrodoc;
     }
+
+	//Obtiene el valor de nombre
     public function getNombre()
     {
         return $this->nombre;
     }
+
+	//Obtiene el valor de apellido
     public function getApellido()
     {
         return $this->apellido;
     }
 
-
-    public function getmensajeoperacion()
+	//Obtiene el valor de mensajeOperacion
+    public function getMensajeOperacion()
     {
-        return $this->mensajeoperacion;
+        return $this->mensajeOperacion;
     }
 
+
+    //! ******** __toString *********
+    public function __toString()
+    {
+        return "\nNombre: " . $this->getNombre() . "\n Apellido:" . $this->getApellido() . "\n DNI: " . $this->getNrodoc() . "\n";
+
+    }
+
+
+	//! ********* BUSCAR *********
     /**
-     * Recupera los datos de una persona por dni
-     * @param int $dni
-     * @return true en caso de encontrar los datos, false en caso contrario
+	 * Recupera los datos de una persona por medio de su numero de dni que se ingresa como parametro.
+	 * Retorna true en caso de encontrar los datos, false en caso contrario.
+	 * @param INT $dni
+	 * @return BOOLEAN $resp 
      */
     public function Buscar($dni)
     {
         $base = new BaseDatos();
-        $consultaPersona = "Select * from persona where nrodoc=" . $dni;
+        $consultaPersona = "Select * from persona where nrodoc = " . $dni;
         $resp = false;
-        if ($base->Iniciar()) {
-            if ($base->Ejecutar($consultaPersona)) {
+
+        //Si se conecta a la base de datos
+        if ($base->Iniciar()){
+
+            //Si se ejecuta la consulta
+            if ($base->Ejecutar($consultaPersona)){
+
+                //
                 if ($row2 = $base->Registro()) {
                     $this->setNrodoc($dni);
                     $this->setNombre($row2['nombre']);
                     $this->setApellido($row2['apellido']);
                     $resp = true;
                 }
-            }else{
-                $this->setmensajeoperacion($base->getError());
+            }else{ //Si no se ejecuta la consulta
+                $this->setMensajeOperacion($base->getError());
             }
-        }else{
-            $this->setmensajeoperacion($base->getError());
+        }else{ //Si no se conecta a la base de datos
+            $this->setMensajeOperacion($base->getError());
         }
         return $resp;
     }
 
+
+    //! ******** LISTAR ******** 
+    /**
+	 * Lista los datos de todas las personas por medio de una consulta ingresada como parametro.
+	 * Retorna un array con los datos obtenidos.
+	 * @param STRING $condicion
+	 * @return ARRAY $arregloPersona 
+	 */		
     public function listar($condicion = "")
     {
+        //Inicializo variables
         $arregloPersona = null;
         $base = new BaseDatos();
+
+        //Asigno valor a la consulta y la trabajo con la condicional if
         $consultaPersonas = "Select * from persona ";
         if ($condicion != "") {
             $consultaPersonas = $consultaPersonas . ' where ' . $condicion;
         }
+
+        //
         $consultaPersonas .= " order by apellido ";
+
         //echo $consultaPersonas;
+        //Si se conecta a la base de datos
         if ($base->Iniciar()) {
+
+            //Si se ejecuta la consulta
             if ($base->Ejecutar($consultaPersonas)) {
                 $arregloPersona = array();
+
+                //
                 while ($row2 = $base->Registro()) {
                     $NroDoc = $row2['nrodoc'];
                     $Nombre = $row2['nombre'];
@@ -109,24 +165,38 @@ class Persona
 
                     $perso = new Persona();
                     $perso->cargar($NroDoc, $Nombre, $Apellido);
+                    //Agrego la persona al array
                     array_push($arregloPersona, $perso);
                 }
-            } else {
-                $this->setmensajeoperacion($base->getError());
+
+            }else{ //Si no se ejecuta la consulta
+                $this->setMensajeOperacion($base->getError());
             }
-        } else {
-            $this->setmensajeoperacion($base->getError());
+
+        }else{ //Si no se conecta a la base de datos
+            $this->setMensajeOperacion($base->getError());
         }
         return $arregloPersona;
     }
 
+
+    //! ******** INSERTAR ******** 
+    /**
+	 * Inserta una nueva persona a la coleccion de personas.
+	 * Retorna true si pudo insertarlo y false en caso contrario.
+	 * @return BOOLEAN $resp 
+	 */		
     public function insertar()
     {
+        //Inicializo variables
         $base = new BaseDatos();
         $resp = false;
+
+        //
         $consultaInsertar = "INSERT INTO persona(nrodoc, apellido, nombre)
 				VALUES (" . $this->getNrodoc() . ",'" . $this->getApellido() . "')";
 
+        //Si se conecta a la base de datos
         if ($base->Iniciar()) {
 
             if ($id = $base->devuelveIDInsercion($consultaInsertar)) {
@@ -134,17 +204,24 @@ class Persona
                 $resp = true;
 
             } else {
-                $this->setmensajeoperacion($base->getError());
+                $this->setMensajeOperacion($base->getError());
 
             }
 
         } else {
-            $this->setmensajeoperacion($base->getError());
+            $this->setMensajeOperacion($base->getError());
 
         }
         return $resp;
     }
 
+
+    //! ******** MODIFICAR ******** 
+    /**
+	 * Modifica una persona de la coleccion de personas.
+	 * Retorna true si pudo modificarla y false en caso contrario.
+	 * @return BOOLEAN $resp 
+	 */		
     public function modificar()
     {
         $resp = false;
@@ -154,38 +231,44 @@ class Persona
             if ($base->Ejecutar($consultaModifica)) {
                 $resp = true;
             } else {
-                $this->setmensajeoperacion($base->getError());
+                $this->setMensajeOperacion($base->getError());
 
             }
         } else {
-            $this->setmensajeoperacion($base->getError());
+            $this->setMensajeOperacion($base->getError());
 
         }
         return $resp;
     }
 
+
+    //! ******** ELIMINAR ******** 
+    /**
+	 * Elimina una persona a la coleccion de personas.
+	 * Retorna true si pudo eliminarlo y false en caso contrario.
+	 * @return BOOLEAN $resp 
+	 */		
     public function eliminar()
     {
         $base = new BaseDatos();
         $resp = false;
-        if ($base->Iniciar()) {
+
+        //Si se conecta a la base de datos
+        if ($base->Iniciar()){
             $consultaBorra = "DELETE FROM persona WHERE nrodoc=" . $this->getNrodoc();
-            if ($base->Ejecutar($consultaBorra)) {
+
+            //Si se ejecuta la consulta
+            if ($base->Ejecutar($consultaBorra)){
                 $resp = true;
-            } else {
-                $this->setmensajeoperacion($base->getError());
 
+            }else{ //Si no puede ejecutar la consulta
+                $this->setMensajeOperacion($base->getError());
             }
-        } else {
-            $this->setmensajeoperacion($base->getError());
 
+        }else{ //Si no puede conectarse a la base de datos
+            $this->setMensajeOperacion($base->getError());
         }
         return $resp;
     }
 
-    public function __toString()
-    {
-        return "\nNombre: " . $this->getNombre() . "\n Apellido:" . $this->getApellido() . "\n DNI: " . $this->getNrodoc() . "\n";
-
-    }
 }
