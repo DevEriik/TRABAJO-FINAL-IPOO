65a669f7be046a3a8e7f78eb08fc5acc42518d18
Ariel-Mamani <arielmamani764@gmail.com>
2024-06-08 00:32:54 -0300

    avances

--- TPFinal/datos/Viaje.php
+++ TPFinal/datos/Viaje.php
@@ -1,5 +1,6 @@
 <?php
 
+
 class Viaje
 {
 
@@ -10,17 +11,18 @@ class Viaje
     private $idempresa;
     private $rnumeroempleado;
     private $vimporte;
+    private $mensajeoperacion;
 
     //!CONSTRUCTOR
 
-    public function __construct($idviaje, $vdestino, $vcantmaxpasajeros, $idempresa, $rnumeroempleado, $vimporte)
+    public function __construct()
     {
-        $this->idviaje = $idviaje;
-        $this->vdestino = $vdestino;
-        $this->vcantmaxpasajeros = $vcantmaxpasajeros;
-        $this->idempresa = $idempresa;
-        $this->rnumeroempleado = $rnumeroempleado;
-        $this->vimporte = $vimporte;
+        $this->idviaje = 0;
+        $this->vdestino = "";
+        $this->vcantmaxpasajeros = 0;
+        $this->idempresa = 0;
+        $this->rnumeroempleado = 0;
+        $this->vimporte = 0;
     }
 
     //! **********GETTERS************
@@ -55,6 +57,11 @@ class Viaje
         return $this->vimporte;
     }
 
+    public function getmensajeoperacion()
+    {
+        return $this->mensajeoperacion;
+    }
+
     //! **********SETTERS*************
 
     public function setidviaje($idviaje)
@@ -87,6 +94,11 @@ class Viaje
         $this->vimporte = $vimporte;
     }
 
+    public function setmensajeoperacion($mensajeoperacion)
+    {
+        $this->mensajeoperacion = $mensajeoperacion;
+    }
+
     //! *********** __toString() ************
 
     public function __toString()
@@ -102,5 +114,142 @@ class Viaje
 
 
     //! ************** METODOS DE MySql ***************
+
+
+    /**
+     * Recupera los datos de una persona por dni
+     * @param int $dni
+     * @return true en caso de encontrar los datos, false en caso contrario
+     */
+    public function Buscar($idviaje)
+    {
+        $base = new BaseDatos();
+        $consultaPersona = "Select * from persona where idviaje=" . $idviaje;
+        $resp = false;
+        if ($base->Iniciar()) {
+            if ($base->Ejecutar($consultaPersona)) {
+                if ($row2 = $base->Registro()) {
+                    $this->setidviaje($row2['idviaje']);
+                    $this->setvdestino($row2['vdestino']);
+                    $this->setvcantmaxpasajeros($row2['vcantmaxpasajeros']);
+                    $this->setidempresa($row2['idempresa']);
+                    $this->setrnumeroempleado($row2['rnumeroempleado']);
+                    $this->setvimporte($row2['vimporte']);
+                    $resp = true;
+                }
+
+            } else {
+                $this->setmensajeoperacion($base->getError());
+
+            }
+        } else {
+            $this->setmensajeoperacion($base->getError());
+
+        }
+        return $resp;
+    }
+
+    public static function listar($condicion=""){
+	    $arregloViaje = null;
+		$base=new BaseDatos();
+		$consultaViaje="Select * from persona ";
+		if ($condicion!=""){
+		    $consultaViaje=$consultaViaje.' where '.$condicion;
+		}
+		$consultaViaje.=" order by destino ";
+		if($base->Iniciar()){
+			if($base->Ejecutar($consultaViaje)){				
+				$arregloViaje= array();
+				while($row2=$base->Registro()){
+				    $idviaje=$row2['idviaje'];
+					$destino=$row2['destino'];
+                    $cantMaxPasajeros=$row2['cantidad maxima'];
+					$idempresa=$row2['idempresa'];
+					$numeroempleado=$row2['numero empleado'];
+                    $importe=$row2['importe'];
+				
+					$viaj=new Viaje();
+					$viaj->cargar($idviaje,$destino,$cantMaxPasajeros,$idempresa,$numeroempleado,$importe);
+					array_push($arregloViaje,$viaj);
+	
+				}
+				
+			
+		 	}	else {
+		 			$this->setmensajeoperacion($base->getError());
+		 		
+			}
+		 }	else {
+		 		$this->setmensajeoperacion($base->getError());
+		 	
+		 }	
+		 return $arregloViaje;
+	}	
+
+
+    /**
+     * Metodo insertar
+     */
+
+    public function insertar()
+    {
+        $base = new BaseDatos();
+        $resp = false;
+        $consultaInsertar = "INSERT INTO viaje(idviaje, vdestino, vcantmaxpasajeros, idempresa, rnumeroempleado, vimporte)
+				VALUES (" . $this->getidviaje() . ",'" . $this->getvdestino() . "','" . $this->getvcantmaxpasajeros() . "','" . $this->getidempresa() . "','" . $this->getrnumeroempleado() . "','" . $this->getvimporte() . "')";
+
+        if ($base->Iniciar()) {
+
+            if ($id = $base->devuelveIDInsercion($consultaInsertar)) {
+                $this->getidviaje($id);
+                $resp = true;
+
+            } else {
+                $this->setmensajeoperacion($base->getError());
+
+            }
+
+        } else {
+            $this->setmensajeoperacion($base->getError());
+
+        }
+        return $resp;
+    }
+
+    /** 
+     * Metodo para Eliminar
+     */
+    
+    public function eliminar()
+    {
+        $base = new BaseDatos();
+        $resp = false;
+        if ($base->Iniciar()) {
+            $consultaBorra = "DELETE FROM viaje WHERE idviaje=" . $this->getidviaje();
+            if ($base->Ejecutar($consultaBorra)) {
+                $resp = true;
+            } else {
+                $this->setmensajeoperacion($base->getError());
+
+            }
+        } else {
+            $this->setmensajeoperacion($base->getError());
+
+        }
+        return $resp;
+    }
+
+    /**
+     * Metodo para cargar viaje
+     */
+    public function cargar($idviaje, $vdestino, $vcantmaxpasajeros, $idempresa, $rnumeroempleado, $vimporte)
+    {
+        $this->setidviaje($idviaje);
+        $this->setvdestino($vdestino);
+        $this->setvcantmaxpasajeros($vcantmaxpasajeros);
+        $this->setidempresa($idempresa);
+        $this->setrnumeroempleado($rnumeroempleado);
+        $this->setvimporte($vimporte);
+    }
 
 }
