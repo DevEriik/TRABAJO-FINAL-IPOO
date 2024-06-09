21f5781c25c9c2c3a80918f17aa870ab51001402
Erick Gonzalez <128252704+DevEriik@users.noreply.github.com>
2024-06-09 00:19:33 -0300

    Include scripts
    Incluimos los scripts en el testViaje.php

--- TPFinal/test/testViajes.php
+++ TPFinal/test/testViajes.php
@@ -1,6 +1,52 @@
 <?php 
+//*Incluimos los scripts. 
+include_once './TPFinal/datos/BaseDatos.php';
+include_once './TPFinal/datos/Empresa.php';
+include_once './TPFinal/datos/ResponsableV.php';
+include_once './TPFinal/datos/Viaje.php';
+include_once './TPFinal/datos/Pasajeros.php';
 
+/**
+ * @param ****************** 
+ * @return *******
+ */
 
+ function nombrefunction(){}
+
+ /**
+ * @param ****************** 
+ * @return *******
+ */
+
+ function nombrefunction(){}
+
+ /**
+ * @param ****************** 
+ * @return *******
+ */
+
+ function nombrefunction(){}
+
+ /**
+ * @param ****************** 
+ * @return *******
+ */
+
+ function nombrefunction(){}
+
+ /**
+ * @param ****************** 
+ * @return *******
+ */
+
+ function nombrefunction(){}
+
+ /**
+ * @param ****************** 
+ * @return *******
+ */
+
+ function nombrefunction(){}
 
 
 
