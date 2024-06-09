a484ae2b46d5bdae7d9e177b31028299afab40ae
Erick Gonzalez <128252704+DevEriik@users.noreply.github.com>
2024-06-09 00:22:19 -0300

    New include
    Me había faltado incluir el script Persona.php

--- TPFinal/test/testViajes.php
+++ TPFinal/test/testViajes.php
@@ -2,6 +2,7 @@
 //*Incluimos los scripts. 
 include_once './TPFinal/datos/BaseDatos.php';
 include_once './TPFinal/datos/Empresa.php';
+include_once './TPFinal/datos/Persona.php';
 include_once './TPFinal/datos/ResponsableV.php';
 include_once './TPFinal/datos/Viaje.php';
 include_once './TPFinal/datos/Pasajeros.php';
